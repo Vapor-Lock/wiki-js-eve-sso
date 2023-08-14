72298531aae6118d11800ca5f058b2c75d2b7a01
slwst <email@slw.st>
2023-08-14 11:24:12 -0400

    feat: authentication via eve esi and user creation now working

--- authentication.js
+++ authentication.js
@@ -9,19 +9,33 @@ const EveOnlineSsoStrategy = require('passport-eveonline-sso').Strategy;
 module.exports = {
   init(passport, conf) {
     console.log(conf)
-    passport.use(conf.key,
-      new EveOnlineSsoStrategy({
-        clientID: conf.clientID,
-        clientSecret: conf.clientSecret,
-        callbackURL: conf.callbackURL,
-      }, function(accessToken, refreshToken, profile, cb) {
-        console.log(profile)
-        WIKI.models.users.processProfile(profile).then((user) => {
-          return cb(null, user) || true
-        }).catch((err) => {
-          return cb(err, null) || true
+    var client = new EveOnlineSsoStrategy({
+      clientID: conf.clientID,
+      clientSecret: conf.clientSecret,
+      callbackURL: conf.callbackURL,
+      passReqToCallback: true,
+    }, async (req, accessToken, refreshToken, profile, cb) => {
+      try {
+        //console.log(profile) //[DEBUG]
+        const user = await WIKI.models.users.processProfile({
+          providerKey: req.params.strategy,
+          profile: {
+            ...profile,
+            id: profile.CharacterID,
+            displayName: profile.CharacterName,
+            email: `${profile.CharacterID}@evesso.local`,
+            picture: `https://images.evetech.net/characters/${profile.CharacterID}/portrait?tenant=tranquility&size=128`
+          }
         })
+        console.log("record created: %s", user)
+        cb(null, user)
+
+      } catch (err) {
+        console.log("failed: %s", err)
+        //console.log("user: %s", user)
+        cb(err, null)
       }
-      ))
+    })
+    passport.use(conf.key, client)
   }
 }
